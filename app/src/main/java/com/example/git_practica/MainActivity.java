511eef99035bb5156366f4ef67f50f478e12516f
Axel Zuri <axelzudri99@gmail.com>
2024-12-04 11:13:35 -0600

    Primer Commit Axel

---
+++ app/src/main/java/com/example/git_practica/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.git_practica;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtName, edtEmail;
+    private Button btnRegister;
+    private TextView tvUserData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+        tvUserData = findViewById(R.id.tvUserData);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = edtName.getText().toString();
+                String email = edtEmail.getText().toString();
+
+                String userData = "Nombre: " + name + "\nCorreo: " + email;
+                tvUserData.setText(userData);
+            }
+        });
+    }
+}
+
+
