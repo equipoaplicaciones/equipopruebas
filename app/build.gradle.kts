52e8d00e32e56272aa63a25b173289d283d50186
Kevin <equipoaplicaciones101@gmail.com>
2025-01-08 23:49:29 -0600

    backend solucionado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")  // Si necesitas convertir JSON a objetos Java
 }
