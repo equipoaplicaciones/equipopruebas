3cb36718f5855a77a4c445a84e89a716ee154c6c
diego <jhortiales0@gmail.com>
2025-01-15 15:27:08 -0600

    fecha y hora

--- app/src/main/java/com/example/git_practica/AgendarCitaActivity.java
+++ app/src/main/java/com/example/git_practica/AgendarCitaActivity.java
@@ -28,7 +28,7 @@ public class AgendarCitaActivity extends AppCompatActivity {
     private EditText editTextNombre, editTextFecha, editTextHora, editTextDescripcion;
     private Button btnGuardarCita;
     private byte[] pdfData; // Variable global para almacenar los datos del PDF descargado
-
+//implementacion del calendario
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
